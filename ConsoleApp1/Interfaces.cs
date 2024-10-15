c99094aef12204e6e3c2832ab0bd41ac2145d019
Tomasz Wsuł <tomasz.wsul@gmail.com>
2024-10-15 22:43:34 +0200

    missing part :D

---
+++ ConsoleApp1/Interfaces.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace ConsoleApp1;
+
+public interface IChromosome;
+public interface IComponent;
+
+public class Genome : List<IChromosome>;
+
+public interface IBlob {}
+
+public interface IBlobBuilder<in T> where T: IComponent
+{
+    bool Apply(Genome g, T c);
+}
+
+public abstract class BlobDirector<TB, TG>
+    where TB : IBlob
+    where TG: Genome
+{
+    public virtual IBlobBuilder<BlobForThisSim>[] Systems { get; }
+
+
+    public virtual BlobForThisSim GetBlob(GenomeForThisSimulation g)
+    {
+        var blob = new BlobForThisSim()
+        {
+            Genome = g,
+        };
+        foreach (var s in Systems)
+        {
+            if (!s.Apply(g, blob))
+            {
+                throw new ApplicationException($"Can't create blob. Failed: {s.GetType().Name}");
+            }
+        }
+
+        return blob;
+    }
+}
---
+++ ConsoleApp1/SimulationSpecific.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1;
+
+public class GenomeForThisSimulation : Genome
+{
+    public GenomeForThisSimulation()
+    {
+        Add(new SpeedChromosome());
+        Add(new MaxAgeChromosome());
+    }
+}
+
+public class DirectoryForThisSim: BlobDirector<BlobForThisSim, GenomeForThisSimulation> {
+    public override IBlobBuilder<BlobForThisSim>[] Systems => [
+        new SpeedBlobSetter<BlobForThisSim>(),
+        new AgeBlobSetter<BlobForThisSim>(),
+    ];
+}
+
+public class BlobForThisSim: IBlob, IMaxAge, IMaxSpeed
+{
+    public GenomeForThisSimulation Genome { get; init; }
+    public int MaxAge { get; set; }
+    public int MaxSpeed { get; set; }
+
+    public override string ToString() => $"Blob(Speed: {MaxSpeed}, MaxAge: {MaxAge})";
+}
